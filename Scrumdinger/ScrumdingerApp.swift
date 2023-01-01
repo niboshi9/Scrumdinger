0e92f406bbacc2ea4749718b4115362deb49a1ae
Shusaku Nishibori <parsleybory@gmail.com>
2023-01-02 01:15:58 +0900

    feat: プロジェクト追加

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by niboshi9 on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
